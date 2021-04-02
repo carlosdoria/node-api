6da25e8167ef034fa12af6779faf56da32a43a70
Carlos Doria <carlosdoria953@gmail.com>
2021-04-02 16:42:02 -0300

    refactor: change variable name

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -21,8 +21,8 @@ export class SignUpController implements Controller {
       }
     }
 
-    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
 
-    if (!emailIsValid) return badRequest(new InvalidParamError('email'))
+    if (!isValidEmail) return badRequest(new InvalidParamError('email'))
   }
 }
